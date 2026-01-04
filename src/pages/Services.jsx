4630126c63bd7296b778358b5ef20c54014d5fe2
Anand Singh <111451218+Michaelanand123singh@users.noreply.github.com>
2026-01-04 14:05:01 +0530

    updated the

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -5,7 +5,6 @@ import DistributionServices from '../components/services/DistributionServices';
 import AntiPiracyServices from '../components/services/AntiPiracyServices';
 import HowWeWork from '../components/services/HowWeWork';
 import ServiceFeatures from '../components/services/ServiceFeatures';
-import PricingSection from '../components/services/PricingSection';
 import SEO from '../components/seo/SEO';
 import { seoData, getServiceSchema } from '../data/seoData';
 
@@ -24,34 +23,34 @@ const Services = () => {
         structuredData={getServiceSchema()}
       />
       <div className="min-h-screen bg-white">
-      <ServicesHero />
-      
-      {/* Service Tabs Section - Always Visible */}
-      <section className="py-16 bg-white border-t border-brown-100/30 relative z-30 -mt-24">
-        <div className="max-w-7xl mx-auto px-4">
-          <ServiceTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+        <ServicesHero />
+        
+        {/* Service Tabs Section - Always Visible */}
+        <section className="py-16 bg-white border-t border-brown-100/30 relative z-30 -mt-24">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6">
+            <ServiceTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+          </div>
+        </section>
+
+        {/* Services Content Section */}
+        <div className="relative z-20">
+          {activeTab === 'distribution' && (
+            <DistributionServices
+              expandedService={expandedService}
+              setExpandedService={handleToggleService}
+            />
+          )}
+          {activeTab === 'antipiracy' && (
+            <AntiPiracyServices
+              expandedService={expandedService}
+              setExpandedService={handleToggleService}
+            />
+          )}
         </div>
-      </section>
 
-      {/* Services Content Section */}
-      <div className="relative z-20">
-        {activeTab === 'distribution' && (
-          <DistributionServices
-            expandedService={expandedService}
-            setExpandedService={handleToggleService}
-          />
-        )}
-        {activeTab === 'antipiracy' && (
-          <AntiPiracyServices
-            expandedService={expandedService}
-            setExpandedService={handleToggleService}
-          />
-        )}
-      </div>
-
-      <HowWeWork />
-      <ServiceFeatures />
-      <PricingSection />
+        {/* Additional Service Information */}
+        <HowWeWork />
+        <ServiceFeatures />
       </div>
     </>
   );
